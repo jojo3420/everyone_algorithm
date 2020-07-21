c13c7976f8414a1421ea803df1ade005e67f3ce4
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-21 16:05:40 +0900

    알고리즘4: 재귀함수 관련 문제 추가   - 1부터 n까지의 합 구하기   - 배열에서 가장 큰 수 구하기

--- src/algorithm-04/index.js
+++ src/algorithm-04/index.js
@@ -28,3 +28,30 @@ function factorial(n) {
 
 console.log(factorial(5) === 120);
 console.log(factorial(10) === 3628800);
+
+
+// 1 부터의 n 까지의 합 구하기 with 재귀함수
+function calcSum(n) {
+  if (n <= 1) return 1;
+  return n + calcSum(n-1);
+}
+
+console.log(calcSum(10) === 55);
+console.log(calcSum(100) === 5050);
+
+
+// 배열 값 중에 최고값 구하기 with 재귀 함수
+function getMax(list, index, max) {
+  if (index === list.length) return max;
+
+  if (max < list[index]) {
+    max = list[index];
+  }
+
+  return getMax(list, index+1, max);
+}
+
+
+console.log(getMax([1, 2, 5, 10, 3, 9], 0, 0) === 10);
+console.log(getMax([4, 110, 220, 1, 5, 3, 9], 0, 0) === 220);
+console.log(getMax([4, 1, 6, 1, 5, 3, 9], 0, 0) === 9);
